222e19bc1f174d9688f398ba286d5d3c6743d206
Colato6 <armijosfer2@gmail.com>
2024-10-28 20:16:43 -0500

    Create Lista.lua

---
+++ Lista.lua
@@ -0,0 +1,17 @@
+local allowedPlayers = {
+    "fernanfloP091o", "armijosfernando2178", "azeldex", "elmegafer",
+    "123daishinkan", "ItzSebaGod", "alexisetter2008",
+    "AlejandroItzi", "TheFinal126",
+    "0oAKILESo0", "brxxn_sl", "GOKUVSJJJ", 
+    "xxXDarknessRisingXxx", "ryu_krs", "mattz678", "FreireBG", "Fernanflop093o", 
+    "Gotenks_129", "InFeRnUsKaSlO", "mattz678",
+    "DEMONZTSB", "rodri2020proxd", "SAHID_YT6792", "FreireBG", "Flux_chog",
+    "robloxesmuymalo2020", "Freire69", "furia3476", "SuperPato0319",
+    "andygamer012345", "Crocrakxer246", "fernando_snake", "R4T4TOPP0",
+    "Gotenks_129", "juancarlosvillo", "CR7_CHAMPIOSN", "FrivUpd",
+    "kayoolicool", "wellington19800", "maisde8milksks", "frandeli0101",
+    "gokumalvado1234", "cepeer_minecratf", "SEBAS_LAPAJ", "santiago123337pro",
+    "Thamersad172", "yere0303", "Ocami7", "jesusxdgggg", "JAVIER_ROBLOX", 
+    "angente6real6", "Jefflop2002", "leonardi4133", "CRACKLITOS_ROBLOX", 
+    "luisgamey28267", "Turufo_1", "aTUJUAN", "ALT_garou11", "BETOKILLER15", "frost123337", "Kasenli"
+}
