a79c71249b6ccd2c668fe4ad905e3d674d55ee34
Colato6 <armijosfer2@gmail.com>
2025-08-21 01:32:44 -0500

    Update and rename Farm.lua to ihm.lua

--- Farm.lua
+++
@@ -1,8 +0,0 @@
-local success, err = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cgggtxcgh-lab/Mong/refs/heads/main/Fri.lua"))()
-end)
-
-if not success then
-    warn("Primer script falló, ejecutando el segundo...")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Cocol1Hh/Soy-del-Campo-no-compartan-script-quien-logr-Desufusco/refs/heads/main/No%20Compartan.lua?token=GHSAT0AAAAAADJTWQMT3EHB36KXSMESCPRU2FGRW3Q"))()
-end
